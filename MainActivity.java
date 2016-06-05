d2cb1742e9dea99bbcb23d6bbdef333803b30d52
AlexL <aspiringls942@gmail.com>
2016-06-04 18:29:42 -0700

    Added WebViewClient, onKeyDown
    Now our browser can browser on its own, without using external browser.

---
+++ MainActivity.java
@@ -0,0 +1,159 @@
+package edu.ucsb.cs.cs185.yan.finalproject185;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.GestureDetector;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.Toast;
+
+//gesture libraries
+import android.view.MotionEvent;
+import android.gesture.Gesture;
+
+import java.net.URL;
+import java.security.Key;
+
+import static android.view.GestureDetector.*;
+
+public class MainActivity extends AppCompatActivity implements OnGestureListener, OnDoubleTapListener {
+
+    WebView webView;
+    private GestureDetector GestureDetect;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.webView);
+        final EditText edittext = (EditText) findViewById(R.id.urlField);
+
+        GestureDetect = new GestureDetector(this, this);
+        GestureDetect.setOnDoubleTapListener(this);
+
+        if (Build.VERSION.SDK_INT >= 19) {
+            webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        }
+
+        edittext.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on key press
+                    Toast.makeText(getApplicationContext(),edittext.getText(), Toast.LENGTH_SHORT).show();
+                    webView.loadUrl(edittext.getText().toString());
+                    webView.setWebViewClient(new myWebViewClient());
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private class myWebViewClient extends WebViewClient{
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView webView, String url){
+            webView.loadUrl(url);
+            return true;
+        }
+
+    }
+
+    @Override
+    public boolean onKeyDown(int KeyCode, KeyEvent event){
+        if((KeyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack())
+        {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(KeyCode, event);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        GestureDetect.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        Toast.makeText(getApplicationContext(), "onSingleTapUp", Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        Toast.makeText(getApplicationContext(), "onDoubleTap", Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        return false;
+    }
+}
